89411b142bf86a6f15858b66114b75707ae1cce5
Yann Lojewski <yann.lojewski@gmail.com>
2025-10-29 23:50:33 +0100

    fix: ensure lint-staged formatting writes changes (#29)

--- packages/lint-staged-config/config.mjs
+++ packages/lint-staged-config/config.mjs
@@ -1,5 +1,8 @@
 export const config = {
-  '*.{js,jsx,ts,tsx,json,md,css,scss,html}': () => 'pnpm lint -- --fix',
+  '*.{js,jsx,ts,tsx,json,md,css,scss,html}': () => [
+    'pnpm lint -- --fix',
+    'pnpm format -- --write',
+  ],
 }
 
 
