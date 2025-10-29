c32bcb7791a554d7f98c3c4cde84e065f24f171c
Yann Lojewski <yann.lojewski@gmail.com>
2025-10-29 23:01:12 +0100

    chore(power): make lint-staged work

--- packages/lint-staged-config/config.mjs
+++ packages/lint-staged-config/config.mjs
@@ -1,6 +1,5 @@
 export const config = {
-  '*.{js,jsx,ts,tsx,json,md,css,scss,html}': {
-    title: 'pnpm lint',
-    task: () => 'pnpm lint'
-  }
+  '*.{js,jsx,ts,tsx,json,md,css,scss,html}': ['pnpm lint -- fix'],
 }
+
+
