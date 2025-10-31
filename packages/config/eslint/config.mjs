c4158065be98b22ef2faa2000551a1e61fbee596
ylojewski <ylojewski@gmail.com>
2025-10-31 13:38:21 +0100

    feat(eslint): enforce boundaries for parent imports

--- packages/config/eslint/config.mjs
+++ packages/config/eslint/config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tsparser from '@typescript-eslint/parser'
 import prettierConfig from 'eslint-config-prettier'
+import boundariesPlugin from 'eslint-plugin-boundaries'
 import importPlugin from 'eslint-plugin-import'
 
 const strictRules = tseslint.configs.strict?.rules ?? {}
@@ -13,11 +14,24 @@ export const ignoreConfig = {
 
 const sharedStyleConfig = {
   plugins: {
-    import: importPlugin
+    import: importPlugin,
+    boundaries: boundariesPlugin
+  },
+  settings: {
+    'boundaries/elements': [
+      {
+        type: 'package',
+        pattern: 'packages/*'
+      },
+      {
+        type: 'app',
+        pattern: 'apps/*'
+      }
+    ]
   },
   rules: {
     'import/first': 'error',
-    'import/no-relative-parent-imports': 'error',
+    'boundaries/no-private': ['error', { allowUncles: false }],
     'import/order': [
       'error',
       {
