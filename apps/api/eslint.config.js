a79a7fcdc185b424e772e27f47ba5847d7cce563
ylojewski <ylojewski@gmail.com>
2025-11-01 17:48:51 +0100

    chore(api): inline eslint tsconfig overrides

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,3 +1,37 @@
-import { config } from '@powercoach/config/eslint'
+import { config, vitestConfig } from '@powercoach/config/eslint'
+import process from 'node:process'
 
-export default config
+const [ignoreConfig, sharedStyleConfig, jsConfig, typescriptConfig, prettierConfig] = config
+
+const apiTypescriptConfig = {
+  ...typescriptConfig,
+  languageOptions: {
+    ...typescriptConfig.languageOptions,
+    parserOptions: {
+      ...(typescriptConfig.languageOptions?.parserOptions ?? {}),
+      project: ['./tsconfig.src.json'],
+      tsconfigRootDir: process.cwd()
+    }
+  }
+}
+
+const apiVitestConfig = {
+  ...vitestConfig,
+  languageOptions: {
+    ...vitestConfig.languageOptions,
+    parserOptions: {
+      ...(vitestConfig.languageOptions?.parserOptions ?? {}),
+      project: ['./tsconfig.test.json'],
+      tsconfigRootDir: process.cwd()
+    }
+  }
+}
+
+export default [
+  ignoreConfig,
+  sharedStyleConfig,
+  jsConfig,
+  apiTypescriptConfig,
+  prettierConfig,
+  apiVitestConfig
+]
