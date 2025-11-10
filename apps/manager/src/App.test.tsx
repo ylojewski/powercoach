bb778867bf9a5c9a928a2013f473f2c06bab3fa2
Yann Lojewski <ylojewski@gmail.com>
2025-11-11 00:56:44 +0100

    fix(manager): change CD work dir

--- apps/manager/src/App.test.tsx
+++ apps/manager/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import { App } from './App'
 
 describe('App', () => {
-  it('shows the logo', () => {
+  it('displays the logo', () => {
     render(<App />)
 
     expect(screen.getByRole('img', { name: /logo/i })).toBeInTheDocument()
