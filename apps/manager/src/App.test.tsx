5a626910dd2e17158ddd444f95a3e48a26e97c74
Yann Lojewski <ylojewski@gmail.com>
2025-11-11 00:45:40 +0100

    fix(manager): add pnpm step in CD

--- apps/manager/src/App.test.tsx
+++ apps/manager/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import { App } from './App'
 
 describe('App', () => {
-  it('displays the logo', () => {
+  it('shows the logo', () => {
     render(<App />)
 
     expect(screen.getByRole('img', { name: /logo/i })).toBeInTheDocument()
