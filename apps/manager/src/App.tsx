f7993701f60ded35c8f37bd88abacc2ccacc4178
Yann Lojewski <ylojewski@gmail.com>
2025-11-17 22:43:53 +0100

    chore(manager): dummy change 2

--- apps/manager/src/App.tsx
+++ apps/manager/src/App.tsx
@@ -17,7 +17,7 @@ export function App(): ReactElement {
     <main style={{ display: 'flex' }}>
       <img alt="logo" src={logo} width={80} />
       <h1>
-        Console Manager, power !
+        Console Manager, power COACH !
         <br />
         {health}
       </h1>
