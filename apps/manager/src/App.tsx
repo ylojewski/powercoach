7fe531ac7e23aeccc4dce254c88a8b4bd6dd67f4
ylojewski <ylojewski@gmail.com>
2025-11-18 13:35:49 +0100

    feat(manager): party (#39)

--- apps/manager/src/App.tsx
+++ apps/manager/src/App.tsx
@@ -17,7 +17,7 @@ export function App(): ReactElement {
     <main style={{ display: 'flex' }}>
       <img alt="logo" src={logo} width={80} />
       <h1>
-        Console Manager, :-) !
+        Powercoach Manager
         <br />
         {health}
       </h1>
