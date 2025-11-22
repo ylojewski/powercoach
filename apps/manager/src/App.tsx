001e947996641fe37760e9ee6919b917f47b074f
Yann Lojewski <ylojewski@gmail.com>
2025-11-22 14:30:40 +0100

    ci: dummy change
    Closes #42
    
    Closes #43

--- apps/manager/src/App.tsx
+++ apps/manager/src/App.tsx
@@ -17,7 +17,7 @@ export function App(): ReactElement {
     <main style={{ display: 'flex' }}>
       <img alt="logo" src={logo} width={80} />
       <h1>
-        Powercoach Manager
+        Powerpouet Manager
         <br />
         {health}
       </h1>
