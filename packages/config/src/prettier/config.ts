06488b98dc53f469f984980065a1171508d7bdd3
Yann Lojewski <ylojewski@gmail.com>
2025-12-21 15:18:07 +0100

    fix(ui): order of exports
    Closes #85
    From fix/ui/order-of-exports

--- packages/config/src/prettier/config.ts
+++ packages/config/src/prettier/config.ts
@@ -1,10 +1,14 @@
 import { type Config } from 'prettier'
 import sortJsonPlugin from 'prettier-plugin-sort-json'
 
+import { packageJsonSortOrder } from './packageJsonSortOrder'
+
 export const config: Config = {
   arrowParens: 'always',
   jsonRecursiveSort: true,
-  overrides: [{ files: 'package.json', options: { parser: 'json' } }],
+  overrides: [
+    { files: 'package.json', options: { jsonSortOrder: packageJsonSortOrder, parser: 'json' } }
+  ],
   plugins: [sortJsonPlugin],
   printWidth: 100,
   semi: false,
